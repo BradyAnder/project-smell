06bb49366fb39e0cf5525508f9c6091e7c083712
BradyAnder <bradyander105@gmail.com>
2024-03-17 10:51:45 -0400

    Added random item generation

---
+++ Project Smell/Assets/Scripts/MapGenerator/ItemSpawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+
+    public int itemSpawnChance;
+
+    public List<GameObject> items = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int ranNumber = Random.Range(0, 100);
+        if(ranNumber <= itemSpawnChance)
+        {
+            int ranItem = Random.Range(0, items.Count);
+            Instantiate(items[ranItem], transform.position, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
