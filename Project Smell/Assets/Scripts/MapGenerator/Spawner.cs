b79c5ff465b5d68f05b7fbc9c067b705878c6544
BradyAnder <bradyander105@gmail.com>
2024-03-16 22:14:25 -0400

    Added inspector headers

--- Project Smell/Assets/Scripts/MapGenerator/Spawner.cs
+++ Project Smell/Assets/Scripts/MapGenerator/Spawner.cs
@@ -2,15 +2,25 @@ using Palmmedia.ReportGenerator.Core;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Reflection;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
     private LandmarkGen landmarkGenScript;
+
+    [Header("Direction")]
+    [Space(2)]
     public bool isNorth = false;
     public bool isEast = false;
     public bool isSouth = false;
     public bool isWest = false;
+    [Space(2)]
+
+    [Header("Destroy on Spawn")]
+    [Space(2)]
+    [Tooltip("Will destroy spawner on game start")]
+    public bool destroy = false;
 
     private void Awake()
     {
@@ -27,18 +37,38 @@ public class Spawner : MonoBehaviour
         if (isNorth == true)
         {
             landmarkGenScript.Generate(transform.position, new Vector3(0,180,0));
+
+            if(destroy == true)
+            {
+                Destroy(gameObject);
+            }
         }
         if (isSouth == true)
         {
             landmarkGenScript.Generate(transform.position, new Vector3(0, 0, 0));
+
+            if (destroy == true)
+            {
+                Destroy(gameObject);
+            }
         }
         if (isEast == true)
         {
             landmarkGenScript.Generate(transform.position, new Vector3(0, -90, 0));
+
+            if (destroy == true)
+            {
+                Destroy(gameObject);
+            }
         }
         if (isWest == true)
         {
             landmarkGenScript.Generate(transform.position, new Vector3(0, 90, 0));
+
+            if (destroy == true)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 
